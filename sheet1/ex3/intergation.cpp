3bf33409b354575835a62aad543e7d4274fa6a07
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2023-11-05 21:05:02 +0100

    Finish sheet1 ex3

--- sheet1/ex3/intergation.cpp
+++ sheet1/ex3/intergation.cpp
@@ -4,11 +4,12 @@
 #define VCL_NAMESPACE VCL
 #include "../../src/vcl/vectorclass.h"
 #include "../../src/time_experiment.hh"
+#include <fstream>
 
-using VecType = VCL::Vec8f; //Vec4d
+using VecType = VCL::Vec4d; // Vec4d
 
-#define LEN_VEC 8 // number of elements in vector, corresponding to VecType
-#define ElEMENT_TYPE float //element type of the vector, corresponding to VecType
+#define LEN_VEC 4           // number of elements in vector, corresponding to VecType
+#define ElEMENT_TYPE double // element type of the vector, corresponding to VecType
 
 using std::cout, std::endl;
 
@@ -41,7 +42,7 @@ public:
     }
 };
 
-double int_midpoint(double a, double b, int n, Function &fPtr)
+double int_midpoint(double a, double b, int n, Function *fPtr)
 {
     double h = (b - a) / n;
     double sum = 0;
@@ -50,7 +51,7 @@ double int_midpoint(double a, double b, int n, Function &fPtr)
     for (int i = 0; i < n; ++i)
     {
         x_mid = a + (i + 0.5) * h;
-        sum += fPtr.eval(x_mid);
+        sum += fPtr->eval(x_mid);
     }
     return sum * h;
 }
@@ -102,7 +103,7 @@ public:
 };
 
 // function overload, differentiate by Function_vec or Function
-double int_midpoint(double a, double b, int n, Function_vec &fVecPtr)
+double int_midpoint(double a, double b, int n, Function_vec *fVecPtr)
 {
     double h = (b - a) / n;
     int turns = n / LEN_VEC;
@@ -121,34 +122,58 @@ double int_midpoint(double a, double b, int n, Function_vec &fVecPtr)
 
         x_mid_vec.load(x_mid_arr);
 
-        sum_vec += fVecPtr.eval(x_mid_vec);
+        sum_vec += fVecPtr->eval(x_mid_vec);
     }
     sum = VCL::horizontal_add(sum_vec);
 
     for (int i = turns * LEN_VEC; i < n; ++i)
     {
         x_mid = a + (i + 0.5) * h;
-        sum += fVecPtr.eval_single(x_mid);
+        sum += fVecPtr->eval_single(x_mid);
     }
     return sum * h;
 }
 
-template<typename FunctionType>
+template <typename FunctionType>
 // package an experiment as a functor
-class Experiment {
+class Experiment
+{
 public:
-  // construct an experiment
-  Experiment (int num_intervals_, FunctionType F_) : num_intervals(num_intervals_),F(F_){}
-  // run an experiment; can be called several times
-  void run () const { int_midpoint(0, 1000, num_intervals, F); }
-  // report number of operations
-  double operations () const {return 0;} // calculate outside, not here because F can be different
+    // construct an experiment
+    Experiment(int num_intervals_, FunctionType *fPtr_) : num_intervals(num_intervals_), fPtr(fPtr_) {}
+    // run an experiment; can be called several times
+    void run() const { int_midpoint(0, 1000, num_intervals, fPtr); }
+    // report number of operations
+    double operations() const { return 0; } // calculate outside, not here because F can be different
 private:
     int num_intervals;
-    FunctionType F;
+    FunctionType *fPtr;
 };
 
+template <typename VectorType>
+bool writeMat(VectorType vec, std::string filename)
+{
+    std::ofstream outFile(filename);
+    if (outFile.is_open())
+    {
+        for (auto line : vec)
+        {
+            for (auto element : line)
+            {
+                outFile << element << " ";
+            }
+            outFile << endl;
+        }
+        outFile.close();
+    }
+    else
+    {
+        std::cerr << "Unable to open file for writing." << std::endl;
+        return false;
+    }
 
+    return true;
+}
 int main(void)
 {
     F1 f1;
@@ -164,11 +189,39 @@ int main(void)
     // cout << int_midpoint(1, 10, 50, f2_vec) << endl;
     // cout << int_midpoint(1, 10, 100, f2) << endl;
     // cout << int_midpoint(1, 10, 100, f2_vec) << endl;
+    std::vector<std::vector<double>> time_mat;
+    std::vector<std::vector<double>> flops_mat;
+    for (int i = 0; i < 25; ++i)
+    {
+        int n = pow(2, i);
+        cout<<i<<" \t"<<n<<endl;
+        int ops_f1 = n * 8 + 3 + (n - 1);
+        int ops_f2 = n * 122 + 3 + (n - 1);
+        Experiment e_f1(n, &f1);
+        Experiment e_f1_vec(n, &f1_vec);
+        Experiment e_f2(n, &f2);
+        Experiment e_f2_vec(n, &f1);
 
-    for (int i = 1; i < 16; ++i){
-        Experiment e_f1(pow(2,i),f1);
-        Experiment e_f1_vec(pow(2,i),f1_vec);
-        Experiment e_f2(pow(2,i),f2);
-        Experiment e_f2_(pow(2,i),f1);
+        auto d1 = time_experiment(e_f1);
+        auto d1_vec = time_experiment(e_f1_vec);
+        auto d2 = time_experiment(e_f2);
+        auto d2_vec = time_experiment(e_f2_vec);
+
+        std::vector<double> time_list = {double(1.0 * d1.second / d1.first),
+                                         double(1.0 * d1_vec.second / d1_vec.first),
+                                         double(1.0 * d2.second / d2.first),
+                                         double(1.0 * d2_vec.second / d2_vec.first)}; //us
+        time_mat.push_back(time_list);
+
+        std::vector<double> flops_list = {
+            d1.first * ops_f1 / d1.second * 1e6 / 1e9,
+            d1_vec.first * ops_f1 / d1_vec.second * 1e6 / 1e9,
+            d2.first * ops_f2 / d2.second * 1e6 / 1e9,
+            d2_vec.first * ops_f2 / d2_vec.second * 1e6 / 1e9,
+        };
+        flops_mat.push_back(flops_list);
     }
+
+    writeMat(time_mat, "time_mat.txt");
+    writeMat(flops_mat, "flops_mat.txt");
 }
